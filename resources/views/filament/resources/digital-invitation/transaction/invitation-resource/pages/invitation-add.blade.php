70063914d59c1447d4f9d20c049f904ec125264a
Antonius Prabowojati <antoniusprabowojati@gmail.com>
2024-08-01 15:32:00 +0700

    Update invitation-add.blade.php

--- resources/views/filament/resources/digital-invitation/transaction/invitation-resource/pages/invitation-add.blade.php
+++ resources/views/filament/resources/digital-invitation/transaction/invitation-resource/pages/invitation-add.blade.php
@@ -14,9 +14,7 @@
         @foreach ($this->getDataku() as $item)
           <li class="mb-3">
             <div class="mx-auto mb-3 flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
-              <div class="shrink-0">
-                <img class="max-w-full h-auto rounded-full" src="{{ url('storage/'.$item->song_image) }}" alt="image" />
-              </div>
+                <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="{{ url('storage/'.$item->song_image) }}" alt="Woman's Face" />
               <div class="w-full">
                 <div class="text-sm font-medium text-black">{{ $item->song_title }}</div>
                 <audio hidden id="file-audio-{{ $item->id }}" src="{{ url('storage/'.$item->song_filename) }}"></audio>
