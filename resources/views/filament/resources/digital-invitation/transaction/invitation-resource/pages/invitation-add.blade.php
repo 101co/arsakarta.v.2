235439f852421decf8c314f2b31471e092ee8cf7
Antonius Prabowojati <antoniusprabowojati@gmail.com>
2024-08-01 15:12:49 +0700

    Update invitation-add.blade.php4

--- resources/views/filament/resources/digital-invitation/transaction/invitation-resource/pages/invitation-add.blade.php
+++ resources/views/filament/resources/digital-invitation/transaction/invitation-resource/pages/invitation-add.blade.php
@@ -24,7 +24,7 @@
               </div>
               <div>
                 <button class="duration-100 ease-in-out active:scale-95" onclick="playMusic(1)" >
-                    <x-heroicon-o-play-circle />
+                    <x-filament::icon-button icon="heroicon-o-play-circle" href="#" tag="a" label="Filament"/>
 <!--                   <svg id="play-audio-button-{{ $item->id }}" type="button" class="size-9 stroke-gray-500 hover:fill-slate-100 active:fill-slate-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
