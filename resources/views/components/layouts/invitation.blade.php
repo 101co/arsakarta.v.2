efce49e43be40b89c64b91404b391f1d5eb2203a
antoniuspj <antoniusprabowojati@gmail.com>
2024-10-25 16:37:01 +0700

    update height content #4

--- resources/views/components/layouts/invitation.blade.php
+++ resources/views/components/layouts/invitation.blade.php
@@ -18,98 +18,91 @@
 
     <style>
         .sk-folding-cube {
-        margin: 20px auto;
-        width: 40px;
-        height: 40px;
-        position: relative;
-        -webkit-transform: rotateZ(45deg);
-                transform: rotateZ(45deg);
+            margin: 20px auto;
+            width: 40px;
+            height: 40px;
+            position: relative;
+            -webkit-transform: rotateZ(45deg);
+                    transform: rotateZ(45deg);
         }
 
         .sk-folding-cube .sk-cube {
-        float: left;
-        width: 50%;
-        height: 50%;
-        position: relative;
-        -webkit-transform: scale(1.1);
-            -ms-transform: scale(1.1);
-                transform: scale(1.1); 
+            float: left;
+            width: 50%;
+            height: 50%;
+            position: relative;
+            -webkit-transform: scale(1.1);
+                -ms-transform: scale(1.1);
+                    transform: scale(1.1); 
         }
         .sk-folding-cube .sk-cube:before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: #333;
-        -webkit-animation: sk-foldCubeAngle 2.4s infinite linear both;
-                animation: sk-foldCubeAngle 2.4s infinite linear both;
-        -webkit-transform-origin: 100% 100%;
-            -ms-transform-origin: 100% 100%;
-                transform-origin: 100% 100%;
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #333;
+            -webkit-animation: sk-foldCubeAngle 2.4s infinite linear both;
+                    animation: sk-foldCubeAngle 2.4s infinite linear both;
+            -webkit-transform-origin: 100% 100%;
+                -ms-transform-origin: 100% 100%;
+                    transform-origin: 100% 100%;
         }
         .sk-folding-cube .sk-cube2 {
-        -webkit-transform: scale(1.1) rotateZ(90deg);
-                transform: scale(1.1) rotateZ(90deg);
+            -webkit-transform: scale(1.1) rotateZ(90deg);
+                    transform: scale(1.1) rotateZ(90deg);
         }
         .sk-folding-cube .sk-cube3 {
-        -webkit-transform: scale(1.1) rotateZ(180deg);
-                transform: scale(1.1) rotateZ(180deg);
+            -webkit-transform: scale(1.1) rotateZ(180deg);
+                    transform: scale(1.1) rotateZ(180deg);
         }
         .sk-folding-cube .sk-cube4 {
-        -webkit-transform: scale(1.1) rotateZ(270deg);
-                transform: scale(1.1) rotateZ(270deg);
+            -webkit-transform: scale(1.1) rotateZ(270deg);
+                    transform: scale(1.1) rotateZ(270deg);
         }
         .sk-folding-cube .sk-cube2:before {
-        -webkit-animation-delay: 0.3s;
-                animation-delay: 0.3s;
+            -webkit-animation-delay: 0.3s;
+                    animation-delay: 0.3s;
         }
         .sk-folding-cube .sk-cube3:before {
-        -webkit-animation-delay: 0.6s;
-                animation-delay: 0.6s; 
+            -webkit-animation-delay: 0.6s;
+                    animation-delay: 0.6s; 
         }
         .sk-folding-cube .sk-cube4:before {
-        -webkit-animation-delay: 0.9s;
-                animation-delay: 0.9s;
+            -webkit-animation-delay: 0.9s;
+                    animation-delay: 0.9s;
         }
         @-webkit-keyframes sk-foldCubeAngle {
-        0%, 10% {
-            -webkit-transform: perspective(140px) rotateX(-180deg);
-                    transform: perspective(140px) rotateX(-180deg);
-            opacity: 0; 
-        } 25%, 75% {
-            -webkit-transform: perspective(140px) rotateX(0deg);
-                    transform: perspective(140px) rotateX(0deg);
-            opacity: 1; 
-        } 90%, 100% {
-            -webkit-transform: perspective(140px) rotateY(180deg);
-                    transform: perspective(140px) rotateY(180deg);
-            opacity: 0; 
-        } 
+            0%, 10% {
+                -webkit-transform: perspective(140px) rotateX(-180deg);
+                        transform: perspective(140px) rotateX(-180deg);
+                opacity: 0; 
+            } 25%, 75% {
+                -webkit-transform: perspective(140px) rotateX(0deg);
+                        transform: perspective(140px) rotateX(0deg);
+                opacity: 1; 
+            } 90%, 100% {
+                -webkit-transform: perspective(140px) rotateY(180deg);
+                        transform: perspective(140px) rotateY(180deg);
+                opacity: 0; 
+            } 
         }
 
         @keyframes sk-foldCubeAngle {
-        0%, 10% {
-            -webkit-transform: perspective(140px) rotateX(-180deg);
-                    transform: perspective(140px) rotateX(-180deg);
-            opacity: 0; 
-        } 25%, 75% {
-            -webkit-transform: perspective(140px) rotateX(0deg);
-                    transform: perspective(140px) rotateX(0deg);
-            opacity: 1; 
-        } 90%, 100% {
-            -webkit-transform: perspective(140px) rotateY(180deg);
-                    transform: perspective(140px) rotateY(180deg);
-            opacity: 0; 
-        }
-        }
-        body {
-            overflow-y: hidden; /* Mencegah scroll vertikal */
-        }
-
-        #content {
-            height: calc(100vh - env(safe-area-inset-bottom));
+            0%, 10% {
+                -webkit-transform: perspective(140px) rotateX(-180deg);
+                        transform: perspective(140px) rotateX(-180deg);
+                opacity: 0; 
+            } 25%, 75% {
+                -webkit-transform: perspective(140px) rotateX(0deg);
+                        transform: perspective(140px) rotateX(0deg);
+                opacity: 1; 
+            } 90%, 100% {
+                -webkit-transform: perspective(140px) rotateY(180deg);
+                        transform: perspective(140px) rotateY(180deg);
+                opacity: 0; 
+            }
         }
     </style>
 </head>
@@ -124,7 +117,7 @@
         </div>
     </div>
 
-    <div id="content" class="max-w-lg px-2 py-2 mx-auto">
+    <div id="content" class="h-[calc(100vh-50px)] max-w-lg px-2 py-2 mx-auto">
         <div class="flex items-center justify-center w-full h-full overflow-hidden bg-white rounded-3xl">
             <div class="w-full h-full">
                 {{ $slot }}
