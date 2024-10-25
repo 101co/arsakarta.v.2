a17cf03ac2b378242a60b4fe75f9bb6cf5e75918
antoniuspj <antoniusprabowojati@gmail.com>
2024-10-25 16:27:37 +0700

    update height content #3

--- resources/views/components/layouts/invitation.blade.php
+++ resources/views/components/layouts/invitation.blade.php
@@ -104,6 +104,13 @@
             opacity: 0; 
         }
         }
+        body {
+            overflow-y: hidden; /* Mencegah scroll vertikal */
+        }
+
+        #content {
+            height: calc(100vh - env(safe-area-inset-bottom));
+        }
     </style>
 </head>
 <body>
@@ -117,13 +124,21 @@
         </div>
     </div>
 
-    <div id="content" class="h-screen max-w-lg px-2 py-2 mx-auto">
+    <div id="content" class="max-w-lg px-2 py-2 mx-auto">
         <div class="flex items-center justify-center w-full h-full overflow-hidden bg-white rounded-3xl">
             <div class="w-full h-full">
                 {{ $slot }}
             </div>
         </div>
     </div>
+
+    {{-- <div id="content" class="h-screen max-w-lg px-2 py-2 mx-auto">
+        <div class="flex items-center justify-center w-full h-full overflow-hidden bg-white rounded-3xl">
+            <div class="w-full h-full">
+                {{ $slot }}
+            </div>
+        </div>
+    </div> --}}
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 
